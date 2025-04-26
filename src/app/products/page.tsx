7d369aafb857d507fc3ca34abfa367de98c61add
yoss <ghostza1209@gmail.com>
2025-04-26 12:08:46 +0700

    feat: enhance contact page, products page, and product-details page with improved layout and localized content

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,25 +1,25 @@
 import ProductSearchPage from "@/app/feature/product/components/ProductSearchPage";
-import { Suspense } from "react";
-import { Metadata } from "next";
-import { baseUrl } from "@/constants";
+import {Suspense} from "react";
+import {Metadata} from "next";
+import {baseUrl} from "@/constants";
 
 export const metadata: Metadata = {
-  title: "Products | Phuket Flower Shop",
-  description: "Browse our collection of beautiful flower arrangements and gifts.",
-  openGraph: {
-    title: "Products | Phuket Flower Shop",
-    description: "Browse our collection of beautiful flower arrangements and gifts.",
-    url: new URL("/products", baseUrl).toString(),
-    siteName: "Phuket Flower Shop",
-    locale: "th_TH",
-    type: "website",
-  },
+    title: `ค้นหาดอกไม้สด ดอกไม้ประดิษฐ์ ดอกไม้ไทย พานพุ่ม ราคาถูก | ส่งเร็วทั่วไทย | Phuket Flower Shop`,
+    description: "ค้นหาดอกไม้สด ดอกไม้ประดิษฐ์ ดอกไม้ไทย พานไหว้ครู พานพุ่ม แจกันดอกไม้ ช่อดอกไม้ พร้อมส่งด่วนทั่วไทย สั่งง่าย ราคาคุ้มสุดๆ",
+    openGraph: {
+        title: `ค้นหาดอกไม้สด ดอกไม้ประดิษฐ์ ดอกไม้ไทย พานพุ่ม ราคาถูก | ส่งเร็วทั่วไทย | Phuket Flower Shop`,
+        description: "ค้นหาดอกไม้สด ดอกไม้ประดิษฐ์ ดอกไม้ไทย พานไหว้ครู พานพุ่ม แจกันดอกไม้ ช่อดอกไม้ พร้อมส่งด่วนทั่วไทย สั่งง่าย ราคาคุ้มสุดๆ",
+        url: new URL("/products", baseUrl).toString(),
+        siteName: "Flower & More shop",
+        locale: "th_TH",
+        type: "website",
+    },
 };
 
 export default function ProductsPage() {
-  return (
-    <Suspense>
-      <ProductSearchPage />
-    </Suspense>
-  );
+    return (
+        <Suspense>
+            <ProductSearchPage/>
+        </Suspense>
+    );
 }
